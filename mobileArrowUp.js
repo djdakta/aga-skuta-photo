dbd9cd7d5965334942329a42fbf9f5a705e535aa
tomekskuta <tomskuta@gmail.com>
2017-11-09 19:23:33 +0100

    back to first function arrow up

--- mobileArrowUp.js
+++ mobileArrowUp.js
@@ -1,30 +1,20 @@
-(function mobileArrowUp() {
-  // const clickHandler = function(event) {
-  //   const tempo = 70;
-  //   let space = window.scrollY;
-  //   function goUp() {
-  //     setTimeout(function() {
-  //       if (space >= tempo) {
-  //         space = space - tempo;
-  //         window.scrollTo(0, space);
-  //         goUp();
-  //       } else {
-  //         space = 0;
-  //         window.scrollTo(0, space);
-  //       }
-  //     }, 1);
-  //   };
-  //   goUp();
-  // }
-  // document.querySelector('.back_to_top').addEventListener('click', clickHandler)
-  const header = document.querySelector('.header');
-  const arrow = docuent.querySelector('.back_to_top')
-  function goUp() {
-    header.classList.add('go_up');
-    setTimeout(() => {
-      header.classList.remove('go_up')
-    }, 500);
-  };
-  arrow.addEventListener('click', goUp());
-};
-)()
+(function backToTop() {
+  const clickHandler = function(event) {
+    const tempo = 70;
+    let space = window.scrollY;
+    function goUp() {
+      setTimeout(function() {
+        if (space >= tempo) {
+          space = space - tempo;
+          window.scrollTo(0, space);
+          goUp();
+        } else {
+          space = 0;
+          window.scrollTo(0, space);
+        }
+      }, 1);
+    };
+    goUp();
+  }
+  document.querySelector('.back_to_top').addEventListener('click', clickHandler)
+})()
